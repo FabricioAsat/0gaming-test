c0eb79768b29b30e404227bf0d130f0f3e5f6631
Fabricio Asat <fabricioasat01@gmail.com>
2024-09-05 17:25:06 -0300

    number cotrol error fixed

--- server/src/random/timestamp/timestamp.middleware.ts
+++ server/src/random/timestamp/timestamp.middleware.ts
@@ -1,9 +1,17 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NestMiddleware,
+} from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Injectable()
 export class TimestampMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: () => void) {
+    if (req.body.limit < 0)
+      throw new HttpException('Número no admitido', HttpStatus.BAD_REQUEST);
+
     const timestamp = new Date().toISOString();
     req['timestamp'] = timestamp;
     next();
